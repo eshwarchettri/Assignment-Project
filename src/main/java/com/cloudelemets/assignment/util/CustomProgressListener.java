303ba482fe7f6f6c73e282eb1a3e21720329904e
eshwarchettri <eshwarchettri02@gmail.com>
2018-10-22 15:40:10 +0530

    Update CustomProgressListener.java
    upload progress

--- src/main/java/com/cloudelemets/assignment/util/CustomProgressListener.java
+++ src/main/java/com/cloudelemets/assignment/util/CustomProgressListener.java
@@ -5,29 +5,33 @@
  */
 package com.cloudelemets.assignment.util;
 
-import java.io.IOException;
-
 import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+import java.io.IOException;
+import java.text.NumberFormat;
 
-public class CustomProgressListener implements MediaHttpUploaderProgressListener {
-          @Override
-	  public void progressChanged(MediaHttpUploader uploader) throws IOException {
-	    switch (uploader.getUploadState()) {
-	      case INITIATION_STARTED:
-	        System.out.println("Initiation has started!");
-	        break;
-	      case INITIATION_COMPLETE:
-	        System.out.println("Initiation is complete!");
-	        break;
-	      case MEDIA_IN_PROGRESS:
-	        System.out.println(uploader.getProgress());
-	        break;
-	      case MEDIA_COMPLETE:
-	        System.out.println("Upload is complete!");
-	    }
-	  }
+/**
+ *
+ * @author netelixir
+ */
+public class FileUploadProgressListener implements MediaHttpUploaderProgressListener {
 
-	
-	}
-
+  @Override
+  public void progressChanged(MediaHttpUploader uploader) throws IOException {
+    switch (uploader.getUploadState()) {
+      case INITIATION_STARTED:
+        View.uploadStatus("Upload Initiation has started.");
+        break;
+      case INITIATION_COMPLETE:
+        View.uploadStatus("Upload Initiation is Complete.");
+        break;
+      case MEDIA_IN_PROGRESS:
+        View.uploadStatus("Upload is In Progress: "
+            + NumberFormat.getPercentInstance().format(uploader.getProgress()));
+        break;
+      case MEDIA_COMPLETE:
+        View.uploadStatus("Upload is Complete!");
+        break;
+    }
+  }
+}
